1fa1826989ddd86537e5fbbce48583bc50a41b0a
michelle-tl <miss_amtl@live.com>
2017-05-06 09:14:56 +0200

    created nav

---
+++ src/Landing.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <div className="App-header">
+          <p className="align-left">prevju</p>
+          <div className="menu">
+          <p >Intervjusidan</p>
+          <p >titel</p>
+          <p >titel2</p>
+        </div>
+        </div> */}
+        <div className="">
+          <p className="App-intro">
+            <h2>Welcome to Prevju</h2>
+          Landing page
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
