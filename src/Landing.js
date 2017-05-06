8a3d296a0cea7028b9bdddd74c48171e9112a543
LukaMrk <lukamrkonjic163@gmail.com>
2017-05-06 15:38:07 +0200

    Fix title iteration 100000

--- src/Landing.js
+++ src/Landing.js
@@ -27,8 +27,8 @@ class Landing extends Component {
     <div className="container">
         <div className="intro-text">
 
-            <div className="intro-lead-in">Looking to hire?</div>
-            <div className="intro-heading">Interviewing made easier, cheaper and faster!/div>
+            <div className="intro-heading">Looking to hire?</div>
+            <div className="intro-lead-in">Interviewing made easier, cheaper and faster!</div>
             <Link to='/createformview' className="page-scroll btn btn-xl">Tell Me More </Link>
 
 
