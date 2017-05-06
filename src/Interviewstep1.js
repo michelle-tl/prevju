2352948fb4f9377c5f05b781c83f6bb9bb2afa3e
michelle-tl <miss_amtl@live.com>
2017-05-06 11:22:37 +0200

    logo to step 1

--- src/Interviewstep1.js
+++ src/Interviewstep1.js
@@ -15,7 +15,8 @@ class Interviewstep1 extends Component {
           <p className="Interview">
             <h2>Choose your interview channel:</h2>
               <div className="interview-alternatives">
-              <span>Intervjusidan 1</span>
+              {/* <span>Intervjusidan 1</span> */}
+              <span className="prevju"  aria-hidden="true"></span>
               <span className="phone-center glyphicon glyphicon-earphone"  aria-hidden="true"></span>
               <span className="skype" aria-hidden="true"></span>
 
